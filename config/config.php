e1378aff46967f96588365fc87f544af6e202a34
badguy <htengemail@163.com>
2015-07-07 11:42:09 +0800

    Create config.php
    增加dhl配置文件

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+$config = array(
+
+    // DHL related settings
+    'dhl' => array(
+        // ID to use to connect to DHL
+        'id' => 'YOUR_ID',//'prasanta',
+        // Password to use to connect to DHL
+        'pass' => 'YOUR_PASS',//'prasanta',
+
+        // Shipper, Billing and Duty Account numbers
+        'shipperAccountNumber' => 'YOUR_shipperAccountNumber',
+        //'billingAccountNumber' => 'YOUR_NUMBER',
+        //'dutyAccountNumber' => 'YOUR_NUMBER',
+    ),
+);
